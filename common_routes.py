de665128c235381dc9b2a06ea8ed05b2e7d2a701
tweekes <tmweekes1@gmail.com>
2019-12-06 17:22:50 -0500

    Troubleshooting env vars most changes to be removed

--- common_routes.py
+++ common_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, render_template
+from os import environ
 
 # Blueprint that will be used to register new common routes for the app
 # common = does not need authentication can be accessed regardless of login
@@ -8,7 +9,8 @@ common = Blueprint('common', __name__, template_folder='templates')
 @common.route('/home')
 @common.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    test = environ.get('TEST_VAR')
+    return render_template('index.html', title='Home', test=test)
 
 @common.route('/about')
 def about():
